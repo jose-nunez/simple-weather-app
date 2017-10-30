0e0cdb80d1f03205b3eb45428194b50fb9519a00
Jose Nunez <josenunezdiaz@gmail.com>
2017-10-30 17:26:19 +1100

    More hexagons

--- build/main.js
+++ build/main.js
@@ -3,10 +3,12 @@ const OPEN_WEATHER_APP_URL = 'https://api.openweathermap.org/data/2.5/weather?un
 
 var getTemp= (location)=>{
 
-	return axios.get(OPEN_WEATHER_APP_URL,{params:{
-		lat:location.lat,
-		lon:location.lng,
-	}}).then(
+	return axios.get(OPEN_WEATHER_APP_URL,{
+		params:
+			(typeof location==="string"? 
+				{q:location} : 
+				{lat:location.lat,lon:location.lng})
+	}).then(
 		(resp)=>{
 			if(resp.data.cod && resp.data.message){
 				throw new Error(resp.data.message);
@@ -29,7 +31,7 @@ var getTemp= (location)=>{
 var getLocation = ()=>{
 	return new Promise((resolve,reject)=>{
 		try{
-			navigator.geolocation.getCurrentPosition(p=>resolve(p))
+			navigator.geolocation.getCurrentPosition(p=>resolve(p),err=>reject(err))
 		}
 		catch(err){
 			reject(err);
@@ -39,12 +41,19 @@ var getLocation = ()=>{
 
 var update = weather=>{
 	var temp=''+Math.round(weather.temp);
-	if(temp.length>2) document.getElementById('temp').style.fontSize = '9vw';
-	else document.getElementById('temp').style.fontSize = '11vw';
+	if(temp.length>2) document.getElementById('temp-h').classList.add('smaller');
+	else document.getElementById('temp-h').classList.remove('smaller');
 	document.getElementById('temp').innerHTML= temp +'&#176;C';
 
-
-	document.getElementById('city').innerHTML= weather.city;
+	if(weather.city){
+		document.getElementById('city-h').classList.remove('smaller');
+		document.getElementById('city').innerHTML= weather.city;
+	}
+	else{
+		document.getElementById('city-h').classList.add('smaller');
+		document.getElementById('city').innerHTML= 'Somewhere in the earth';
+	}
+	
 	document.getElementById('icon').setAttribute('class','wi wi-owm-'+weather.time+'-'+weather.icon);
 }
 
@@ -54,10 +63,14 @@ var parsePosition = (lat,lng)=>({lat:lat,lng:lng})
 var run = position=>{
 	(position? 
 		Promise.resolve(position) : 
-		getLocation().then(p=>({lat:p.coords.latitude,lng:p.coords.longitude}))
+		getLocation().then(p=>({lat:p.coords.latitude,lng:p.coords.longitude})).catch(err=>'Santiago')
 	)
 		.then(p=>getTemp(p))
 		.then(r=>update(r));
 }
 
 run();
+// run(parsePosition(76.438545, 104.304871));
+// run(parsePosition(51.083564, 73.786229));
+// run('santiago')
+// run('wellington')
